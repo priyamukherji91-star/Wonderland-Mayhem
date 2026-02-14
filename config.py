b0301115127daa5b43bced75f97e9444b5ba0a06
Cookiechan <priya.mukherji91@gmail.com>
2026-02-14 11:10:03 +0100

    Fix HERE_THEN_GONE channel ID

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ ST_TIMEZONE: Final[str] = "Europe/Luxembourg"
 # ─────────────────────────────────────────────
 
 GATE_CHANNEL_ID: Final[int] = 1251693839962607675          # #choose-your-door
-HERE_THEN_GONE_CHANNEL_ID: Final[int] = 1251693839962607675  # #here-then-gone
+HERE_THEN_GONE_CHANNEL_ID: Final[int] = 1441321146820591728  # #here-then-gone
 ROLES_CHANNEL_ID: Final[int] = 1251693839962607675         # #pick-your-roles
 
 GIVEAWAYS_CHANNEL_ID: Final[int] = 1251693839962607675     # #giveaways
